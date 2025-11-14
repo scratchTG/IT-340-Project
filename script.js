96ed8a4adf3e4c6bc8535a7308f7f62e71d76859
scratchTG <knd@njit.edu>
2025-11-14 11:12:42 -0500

    Milestone 1 frontend login page

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Login front-end working! (Back-end will be added in Milestone 2)");
+});
